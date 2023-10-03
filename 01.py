ad4d363600c163859e7d1207295bdaadbfd1b119
Ganga Devi G S <gangavgpotti@gmail.com>
2023-10-03 13:16:54 +0530

    firt git work

---
+++ 01.py
@@ -0,0 +1,8 @@
+#First Git work
+#Small python program
+num1=int(input("Enter First Number : "))
+num2=int(input("Enter Second Number : "))
+#Let's do some mathamatical problems
+Add = num1+num2
+print("The total of",num1,"and",num2,"is",Add)
+
