4862685f413715dfc5d01688a52529cbe54ffe55
Ganga Devi G S <gangavgpotti@gmail.com>
2023-10-03 13:34:38 +0530

    done other mathematical function

--- 01.py
+++ 01.py
@@ -5,4 +5,13 @@ num2=int(input("Enter Second Number : "))
 #Let's do some mathamatical problems
 Add = num1+num2
 print("The total of",num1,"and",num2,"is",Add)
+print("\n")
+subtract = num1-num2
+print("The subtract value of", num1 ,"and", num2, "is", subtract )
+print("\n")
+multiply =num1*num2
+print("The multiply of ", num1, "and", num2,"is", multiply)
+print("\n")
+divide=num1/num2
+print(f"The diviton of {num1} and {num2} is {divide}" )
 
